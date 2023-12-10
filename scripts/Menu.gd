acf13a7268d67f00d2a7a68856b498860387e942
lnsalaza <lnsalaza@espol.edu.ec>
2023-12-10 16:46:39 -0500

    Paused menu

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxMenu/VBoxBotones/StartGameButton.grab_focus()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
