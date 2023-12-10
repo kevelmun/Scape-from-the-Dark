5bb01d183f8c98a616bf1cb820e43d631c5027e7
lnsalaza <lnsalaza@espol.edu.ec>
2023-12-10 15:22:09 -0500

    Button's style fixed

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/StartGameButton.grab_focus()
+	$VBoxMenu/VBoxBotones/StartGameButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
